b95e0a4c0b2e3b2c7c306a718756c58a0effb9a1
Aidan Gonzales <agonza31@nd.edu>
2019-10-10 16:02:40 -0400

    edited README.md to list which files are for which problem; updated sortgendexp.sh to remove duplicate lines and updated comments.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # IBC_Exercise_05
 Files for Exercise 05 
+
+Problem 1
+sortgendexp.sh is a shell command that creates genderexp.txt. genderexp.txt contains the unique gender-yearsExperience combinations found in wages.csv.
+
+Problem 2
+sortwages.csv is a shell script that gives the desired output.
+
+Problem 3
+gradwages.csv is a shell script that gives the desired output.
--- sortgendexp.sh
+++ sortgendexp.sh
@@ -1,4 +1,4 @@
 # Usage: bash sortgendexp.sh
 
-#Display wages.csv; get rid of the first line; get the Column1(gender)and Column2(yearsExperience); rows should be sorted first by gender and then by yearsExperience; convert "," to " "; store the output into genderexp.txt.
-cat wages.csv | grep -v "gen" | cut -d, -f1-2 | sort -t, -k1,1 -k2,2n | tr ',' ' ' > genderexp.txt
+#Display wages.csv; get rid of the first line; get the Column1(gender)and Column2(yearsExperience); rows should be sorted first by gender and then by yearsExperience; convert "," to " "; store the output into genderexp.txt. ; removes duplicate rows
+cat wages.csv | grep -v "gen" | cut -d, -f1-2 | sort -t, -k1,1 -k2,2n | tr ',' ' ' | uniq > genderexp.txt
