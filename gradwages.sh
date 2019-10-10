8bbf5755a05095947233ab2e956f4d130fc9d955
Aidan Gonzales <agonza31@nd.edu>
2019-10-10 11:13:37 -0400

    updated solution to problem 3 that outputs difference of lowest wage earners for 12 and 16 years of school

--- gradwages.sh
+++ gradwages.sh
@@ -1,12 +1,13 @@
 # Usage: gradwages.sh 12 16
 
 #The minimum wages of 12 years
-cat wages.csv | cut -d , -f 3,4 | grep  "$1," | sort -n -k 2 -t , | head -n 1 | cut -d , -f 2
-
+val1=$(cat wages.csv | cut -d , -f 3,4 | grep  "$1," | sort -n -k 2 -t , | head -n 1 | cut -d , -f 2)
+echo -e "The lowest wage earner with $1 years of school earned: \n $val1 \n" 
 
 #The minimum wages of 16 years
-cat wages.csv | cut -d , -f 3,4 | grep  "$2," | sort -n -k 2 -t , | head -n 1 | cut -d , -f 2
+val2=$(cat wages.csv | cut -d , -f 3,4 | grep  "$2," | sort -n -k 2 -t , | head -n 1 | cut -d , -f 2)
+echo -e "The lowest wage earner with $2 years of school earned: \n $val2 \n" 
 
 #The effect of graduating college ($1 vs. $2 years of school) of minimum wage for earners in this dataset
-
-#echo "$val1 - $val2" | bc????? there is an error for this argument
+echo "The difference between these two wages:"
+echo "$val1 - $val2" | bc
